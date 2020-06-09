c010f8a9d37613761d98a257c19e2ee62e2bf73d
IgorZ07 <wow0707@gmail.com>
2020-06-09 15:02:31 +0200

    VStream backend initialized with nodejs npm run server to start

---
+++ index.js
@@ -0,0 +1,27 @@
+const cv = require('opencv4nodejs');
+const path = require('path');
+const app = require('express')();
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+
+const wCap = new cv.VideoCapture(0);
+
+
+app.get('/', (req, res) => {
+  console.log('object');
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+setInterval(() => {
+  console.log(wCap);
+  wCap.open(0);
+  const frame = wCap.read();
+  const image = cv.imencode('.jpg', frame).toString('base64')
+  io.emit('image', 'image');
+}, 1000)
+
+const PORT = process.env.PORT || 3000; 
+
+http.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+} )
