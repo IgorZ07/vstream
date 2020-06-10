e8a4a214eb82accc2a541b9de705cd7e99d81b4a
IgorZ07 <wow0707@gmail.com>
2020-06-10 11:15:45 +0200

    Draw line received from server

--- static/client.js
+++ static/client.js
@@ -28,3 +28,11 @@ document.addEventListener("DOMContentLoaded", () => {
      mouse.pos.y = e.clientY / height;
      mouse.move = true;
   };
+  // draw line received from server
+ socket.on('draw_line', (data) => {
+     let line = data.line;
+     context.beginPath();
+     context.moveTo(line[0].x * width, line[0].y * height);
+     context.lineTo(line[1].x * width, line[1].y * height);
+     context.stroke();
+  });
