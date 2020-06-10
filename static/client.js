85825c43bb90e1535ae3870c7848a0732133b795
IgorZ07 <wow0707@gmail.com>
2020-06-10 11:13:59 +0200

    Getting  canvas element and create  2D drawing context that we need to draw

--- static/client.js
+++ static/client.js
@@ -5,3 +5,11 @@ document.addEventListener("DOMContentLoaded", () => {
      pos: {x:0, y:0},
      pos_prev: false
   };
+  // get canvas element and create context
+  let canvas  = document.getElementById('drawing');
+  let context = canvas.getContext('2d');
+  let width   = 790;
+  let height  = 510;
+//   let width   = window.innerWidth;
+//   let height  = window.innerHeight;
+  let socket  = io.connect();
